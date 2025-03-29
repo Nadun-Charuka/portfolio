ed7681357a70e2640025b7883a24cb9b52c51c1b
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-03-29 16:01:32 +0530

    services done

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 
 
 
@@ -14,6 +15,7 @@ export default function Home() {
     <Navbar/>
     <Header/>
     <About/>
+    <Services/>
 
     </>
 
