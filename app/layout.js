74dc8b78af74ee358a478d5cfee41dde7871bbdc
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-03-29 11:53:04 +0530

    header done

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${outfit.className} antialiased`}
+        className={`${outfit.className} antialiased leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
